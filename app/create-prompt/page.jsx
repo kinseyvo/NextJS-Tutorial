25b1d1a74a6abc3c8bb9e76dd95bbea897e0ea6f
kinseyvo <kinseyvo@csu.fullerton.edu>
2024-08-07 17:39:23 -0700

    profile page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -8,6 +8,8 @@ import Form from '@components/Form';
 import { Router } from 'next/router';
 
 const CreatePrompt = () => {
+    const { data: session } = useSession();
+
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
@@ -53,10 +55,4 @@ const CreatePrompt = () => {
 export default CreatePrompt
 
 // https://www.youtube.com/watch?v=wm5gMKuwSYk
-// left off 2:12:12
-
-// random links
-// https://mui.com/material-ui/react-slider/
-// https://console.cloud.google.com/apis/credentials?project=promptopia-431119
-// https://cloud.mongodb.com/v2/66aaafe27e989d6e29bb36d4#/metrics/replicaSet/66aab04da7c10c4f23248c76/explorer/share_prompt/users/find 
-// https://learn.svelte.dev/tutorial/welcome-to-svelte
+// left off 2:48:20
